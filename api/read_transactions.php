b53cf3d96660ee6be15b690caa6c5715bfc7b30a
Titou-Dh <titoudh1@gmail.com>
2024-04-18 01:23:23 +0100

    created new api for read data

---
+++ api/read_transactions.php
@@ -0,0 +1,49 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "masroufi";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT DATE_FORMAT(payment_date, '%d %M') AS month, name_payement AS name, 'payé' AS type, -amount AS price FROM payments UNION ALL SELECT DATE_FORMAT(add_date, '%d %M') AS month, add_name AS name, 'ajout' AS type, amount AS price FROM adds ORDER BY month DESC;";
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $transactions = array();
+    $i = 0;
+    while ($row = mysqli_fetch_assoc($result)) {
+        $transactions[$i][] = array(
+            'name' => $row['name'],
+            'type' => $row['type'],
+            'price' => $row['price']
+        );
+        $i++;
+    }
+
+    foreach ($transactions as $date => $transaction) {
+        
+        foreach ($transaction as $trans) {
+            echo ("
+                <div class='item my-3 d-flex justify-content-between align-items-center px-2'>
+                    <div class='d-flex justify-content-between align-items-center'>
+                        <div class='d-flex flex-column  justify-content-center'>
+                            <div class='item-title'>" . $trans['name'] . "</div>
+                            <div class='type-transaction'>". $trans['type'] ."</div>
+                        </div>
+                    </div>
+                    <div class='payement'>". $trans['price'] ."</div>
+                </div>
+                
+            ");
+    }}
+} else {
+    echo "No transactions found";
+}
+
+mysqli_close($conn);
